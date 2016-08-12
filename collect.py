5c00a5289b7b852c0f1c026d4381f1fa2b6743f8
Max Hutchinson <maxhutch@gmail.com>
2016-08-12 16:11:27 -0500

    Tuning collect to a single property.

--- collect.py
+++ collect.py
@@ -2,39 +2,49 @@
 import time
 from citrination_client import CitrinationClient
 from json import dumps, dump
+from string import rstrip
 
 with open("./.api_key", "r") as f:
     key = f.readline().split()[0]
 client = CitrinationClient(key, 'http://citrination.com/')
 
-size = 1
-start = 0
-max_records = 100
-records = []
-while size > 0 and start < max_records:
-  #r = client.search(property="Thermoelectric figure of merit (zT)", from_record=start, per_page=100)
-  r = client.search(property="Internal energy", from_record=start, per_page=100)
-  size = len(r.json()['results'])
-  records += (r.json()['results'])
-  start += size
-  print "Adding data: ", size
-  time.sleep(3) # Remember to sleep to avoid rate limiting!
-print "Found ", str(start), " records"
+with open("./properties.lst", "r") as f:
+    properties_in = f.readlines()
 
-all_keys = []
+properties = [rstrip(x) for x in properties_in if x[0] != "#"]
+
+start_prop = "Density"
+index = properties.index(start_prop)
+properties = properties[index:]
+
+def collect_property(prop):
+    size = 1
+    start = 0
+    max_records = 20000000
+    records = []
+    print "Searching for " + prop
+    while size > 0 and start < max_records:
+        r = client.search(property=prop, from_record=start, per_page=100)
+        size = len(r.json()['results'])
+        records += (r.json()['results'])
+        start += size
+        time.sleep(3) # Remember to sleep to avoid rate limiting!
+        print ".",
+    print "\nCollected " + str(start) + " records for " + prop
+    return records
+
+for prop in properties:
+    records = collect_property(prop)
+    with open(prop.replace(" ","") + ".json", "w") as f:
+        dump(records, f)
+
+"""
+record_contents = []
 for j in range(len(records)):
+    names = []
     for i in range(len(records[j]['sample']['measurement'])):
-        all_keys.append(records[j]['sample']['measurement'][i]['property']['name'])
-
-counts = {}
-for k in all_keys:
-    if k in counts:
-        counts[k] += 1
-    else:
-        counts[k] = 1
-
+        names.append(records[j]['sample']['measurement'][i]['property']['name'])
+    record_contents.append(names)
 """
-print dumps(counts, indent=2)
-with open("counts.json", "w") as f:
-    dump(counts, f)
-"""
+
+
